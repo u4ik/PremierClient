63d2ca55ae47a67b705e23f3f4f03452615229ca
u4ik <affirmativeamit@gmail.com>
2020-06-24 16:42:37 -0400

    fixed nav

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-
-=======
->>>>>>> 7cf9c1c9f016600c56f4f18fb4494ae0c5ddb3c9
 
 class Home extends React.Component {
     render() {
         return (
             <div>
-<<<<<<< HEAD
-           
-=======
                 
->>>>>>> 7cf9c1c9f016600c56f4f18fb4494ae0c5ddb3c9
             </div>
         );
     }
