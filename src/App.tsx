b010a2b00d3d6ea8243781783c6abb84b29c045d
u4ik <affirmativeamit@gmail.com>
2020-06-27 16:24:29 -0400

    flex icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,21 @@ import './App.css';
 import Sitebar from './Components/Nav/Sitebar';
 import Home from './Components/Home/Home';
 import Footer from './Components/Footer/Footer';
-import AuthForm from './Components/Auth/Auth'
+
 
 
 const App: React.FunctionComponent = () => {
 
-  const [updateToken, setUpdateToken] = useState<string>('')
-  const [showAuth, setShowAuth] = useState<boolean>(false);
-  const [signup, setSignup] = useState<boolean>(false);
-  const [signedIn, setSignedIn] = useState<boolean>(true);
-  const [isAdmin, setIsAdmin] = useState(true);
+  // const [updateToken, setUpdateToken] = useState<string>('')
+  // const [showAuth, setShowAuth] = useState<boolean>(false);
+  // const [signup, setSignup] = useState<boolean>(false);
+  // const [signedIn, setSignedIn] = useState<boolean>(false);
+  // const [isAdmin, setIsAdmin] = useState<boolean>(false);
 
   return (
     <div className="App" >
  
-      <Sitebar signedIn={signedIn} setSignedIn={setSignedIn} showAuth={showAuth} setShowAuth={setShowAuth} 
-      updateToken={updateToken} setUpdateToken={setUpdateToken}  signup={signup} setSignup={setSignup} isAdmin={isAdmin} setIsAdmin={setIsAdmin}/>
+      <Sitebar  />
       <Home />
 
       <Footer />
