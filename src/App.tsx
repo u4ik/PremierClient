01f26398b8e2c057fb71e8124a47d881ccc05204
oladelematthew <49046375+oladelematthew@users.noreply.github.com>
2020-06-27 12:56:41 -0400

    more components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Sitebar from './Components/Nav/Sitebar';
 import Home from './Components/Home/Home';
@@ -7,12 +7,19 @@ import AuthForm from './Components/Auth/Auth'
 
 
 const App: React.FunctionComponent = () => {
+
+  const [updateToken, setUpdateToken] = useState<string>('')
+  const [showAuth, setShowAuth] = useState<boolean>(false);
+  const [signup, setSignup] = useState<boolean>(false);
+  const [signedIn, setSignedIn] = useState<boolean>(true);
+  const [isAdmin, setIsAdmin] = useState(true);
+
   return (
     <div className="App" >
  
-      <Sitebar />
+      <Sitebar signedIn={signedIn} setSignedIn={setSignedIn} showAuth={showAuth} setShowAuth={setShowAuth} 
+      updateToken={updateToken} setUpdateToken={setUpdateToken}  signup={signup} setSignup={setSignup} isAdmin={isAdmin} setIsAdmin={setIsAdmin}/>
       <Home />
-      {/* <AuthForm/> */}
 
       <Footer />
       
--- src/Components/Auth/Auth.tsx
+++ src/Components/Auth/Auth.tsx
@@ -1,22 +1,58 @@
 import React, { useState } from 'react';
-import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import {Button, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
+import './Auth.css';
+import Logo from '../../assets/Premier-Commercial-Services-icon.svg';
 
 const baseURL = 'http://localhost:3000/user';
 
+interface displayAuthForm {
+    showAuth: boolean
+    setShowAuth: any
+    updateToken: any
+    setUpdateToken: any
+    signup: any
+    setSignup: any
+}
 
-const Auth:React.FunctionComponent<{ props?: any }> = ({props}) => {
+const modalHeaderStyle:React.CSSProperties= {
+    backgroundColor:'#177BBD',
+    color: 'white',
+    textShadow: '.1rem .1rem .1rem black',
+    borderColor:'transparent',
+    borderRadius: '20px 20px 0px 0px',
+
+} 
+
+const modalFooterStyle:React.CSSProperties= {
+    backgroundColor:'#177BBD',
+    color: 'white',
+    textShadow: '.1rem .1rem .1rem black',
+    borderColor:'transparent',
+    borderRadius: '0px 0px 20px 20px'
+} 
+
+
+const Auth: React.FunctionComponent<displayAuthForm> = (props:displayAuthForm) => {
+
+    const [modal, setModal] = useState(true);
+    
+    const toggle = () => props.setShowAuth(!props.showAuth);
+
+    
+
+
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [location, setLocation] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
-    const [isAdmin, setIsAdmin] = useState(false);
-    const [signup, setSignup] = useState(true);
+   
+    
 
     const submit = (e: any) => {
         e.preventDefault();
-        const url = signup ? baseURL + '/signup' : baseURL + '/signin';
+        const url = props.signup ? baseURL + '/signup' : baseURL + '/signin';
         const reqBody = {
                 firstName: firstName,
                 lastName: lastName,
@@ -24,7 +60,7 @@ const Auth:React.FunctionComponent<{ props?: any }> = ({props}) => {
                 passwordhash: password,
                 location: location,
                 phoneNumber: phoneNumber,
-                isAdmin: isAdmin
+                isAdmin: ''
 
         };
 
@@ -43,42 +79,64 @@ const Auth:React.FunctionComponent<{ props?: any }> = ({props}) => {
 
     return (
             <div>
-                <Form onSubmit={e => submit(e)}>
-                    <h3>{signup ? 'Sign Up' : 'Sign In'}</h3>
+
+                <Modal style={{borderRadius:'20px'}} isOpen={props.showAuth} toggle={toggle} className=''>
+                    <ModalHeader toggle={toggle} style={modalHeaderStyle}>
+                    
+                    <img src = {Logo} style={{width:'20%'}} />
+                    </ModalHeader>
+                    <ModalBody style={{backgroundColor: '#009FE4'}}>
+                        <Form>
+                            <h3>{props.signup ? 'Sign Up' : 'Sign In'}</h3>
+                          
+                            <FormGroup>
+                                <Label htmlFor="email">Email</Label>
+                                <Input name="email" value={email} onChange={e => setEmail(e.target.value)} required />
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} required />
+                            </FormGroup>
+
+                            {props.signup ? 
+                            <div>
+                            <FormGroup>
+                                <Label htmlFor="firstName">First Name</Label>
+                                <Input name="firstName" value={firstName} onChange={e => setFirstName(e.target.value)} required />
+                            </FormGroup>
+                            
+
+                            <FormGroup>
+                                <Label htmlFor="lastName">Last Name</Label>
+                                <Input name="lastName" value={lastName} onChange={e => setLastName(e.target.value)} required />
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Label htmlFor="location">Location</Label>
+                                <Input type="text" name="location" value={location} onChange={e => setLocation(e.target.value)} required />
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Label htmlFor="phoneNumber">Phone Number</Label>
+                                <Input type="number" name="phoneNumber" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} required />
+                            </FormGroup>
+                            </div>
+                            : null}
+                            </Form>
                         
-                    <FormGroup>
-                        <Label htmlFor="firstName">First Name</Label>
-                        <Input name="firstName" value={firstName} onChange={e => setFirstName(e.target.value)} required />
-                    </FormGroup>
+                    </ModalBody>
+                    <ModalFooter style={modalFooterStyle}>
+                    <Button type="button" onClick={() => props.setSignup(!props.signup)}>{props.signup ? 'Have an account? Signin' : 'Need an account? Signup!'}</Button>
+                        <Button color="primary" onClick={(e) => {
+                            toggle()
+                            submit(e)}}>Submit</Button>{' '}
+                        
+                    </ModalFooter>
+                    
+            </Modal>
 
-                    <FormGroup>
-                        <Label htmlFor="lastName">Last Name</Label>
-                        <Input name="lastName" value={lastName} onChange={e => setLastName(e.target.value)} required />
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label htmlFor="email">Email</Label>
-                        <Input name="email" value={email} onChange={e => setEmail(e.target.value)} required />
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label htmlFor="password">Password</Label>
-                        <Input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} required />
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label htmlFor="location">Location</Label>
-                        <Input type="text" name="location" value={location} onChange={e => setLocation(e.target.value)} required />
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label htmlFor="phoneNumber">Phone Number</Label>
-                        <Input type="number" name="phoneNumber" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} required />
-                    </FormGroup>
-
-                    <Button type="button" onClick={() => setSignup(!signup)}>{signup ? 'Have an account? Signin' : 'Need an account? Signup!'}</Button>
-                    <Button type="submit">Submit</Button>
-                </Form>
+                
                 
             </div>
         );
