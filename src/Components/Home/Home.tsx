61cdfff28b5a736a0e8403ed4ec99b3ea398192f
u4ik <affirmativeamit@gmail.com>
2020-07-25 02:54:51 -0400

    design additional functionality

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -11,7 +11,15 @@ import './Home.css'
 
 // import LogoLayer from '../../assets/logoLayer.png'
 
-class Home extends React.Component {
+
+type HomeProps = {
+    signedIn: boolean
+
+}
+
+
+
+class Home extends React.Component <HomeProps, unknown> {
     render() {
         return (
             <div style={{height:"auto", backgroundImage: `url(${HomeBackground})`, backgroundRepeat: '', backgroundPosition: 'center', backgroundSize: 'cover'}}>
@@ -26,8 +34,11 @@ class Home extends React.Component {
 
                             <Col className="welcomeText">
                             <img src={LogoIcon} style={{width: '10vh', marginBottom: '1rem',  filter:'drop-shadow(2px 2px 1px black)'}}></img>
+                            {this.props.signedIn ? 
                             <h4 style={{fontSize:'1.9rem', textShadow:'2px 2px 1px black', color:'#E8C10D', userSelect:'none' }}>
-                                Welcome!</h4>
+                                Welcome&nbsp;{localStorage.getItem('firstname')}!</h4>
+                            :    <h4 style={{fontSize:'1.9rem', textShadow:'2px 2px 1px black', color:'#E8C10D', userSelect:'none' }}>
+                            Welcome!</h4> }
                             <p className='welcomeText' style={{ fontSize: '1.1rem', textShadow:'2px 2px 1px black', lineHeight:'3rem', color: 'white'}}>
                                 In simplest terms, Premier Commercial Services provides a refreshingly reliable and unique approach to providing basic facilities management resources. Our foundational services are janitorial/cleaning, light maintenance and grounds keeping.
                         Our approach to customer accountability and collaboration are causing businesses to leave their traditional facilities support services and switch to Premier. In exchange, they partner with a professional and reliable resource that eliminates the headaches often related to this type of service.</p>
