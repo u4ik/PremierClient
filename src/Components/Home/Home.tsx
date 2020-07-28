e35f90e2c5db3ed70f72f26cdf5c517dfb2228b0
u4ik <affirmativeamit@gmail.com>
2020-07-28 05:55:53 -0400

    Updated Animations/TestimonialHomeScrollFix/After a user signs up, they will go to login!

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -17,6 +17,7 @@ import {Spring, animated} from 'react-spring/renderprops'
 
 
 
+import LoadingGif from '../../assets/premier-icon.png'
 
 
 
@@ -141,177 +142,182 @@ class Home extends React.Component <HomeProps, HomeTypes> {
         return (
             
             <div style={{height:"auto", minHeight:'inherit', backgroundImage: `url(${HomeBackground})`, backgroundRepeat: '', backgroundPosition: 'center', backgroundSize: 'cover',  paddingBottom:'3%'}}>
-                <div style={{ display: 'flex', flexDirection:'row', justifyContent:'center', paddingBottom:'3%',paddingTop:'5vh'}} >
-                    
-                <Spring
-                            config={{duration: 700}} 
-                            native
-                            from={{ o: 0, marginT: '-1500px' }}
-                            to={{ o: 1, marginT: '0px'  }} 
-                            >
-                            {({ o, marginT }) => (
-                            <animated.div style={{
-                                    opacity: o,
-                                    marginTop: marginT
-                                    
-                            }}>
-                    
-                    <div className="transparentContainer" style={{display:'flex',flexDirection: 'column',  marginLeft:'2%',marginRight:'2%',  background: 'rgba(26, 35, 64, 0.5)', borderRadius:'20px'}}>
-                        <div className="welcomeText" style={{padding:'2%', textAlign:'center'}} >
-
-                       
-                            <Container>
-                                <Col className="welcomeText">
-                            <img className="logoIcon" src={LogoIcon} style={{width: '10vh', marginBottom: '1rem', marginTop: '4%',  filter:'drop-shadow(2px 2px 1px black)'}}></img>
-                            {this.props.signedIn ? 
-                            <h4  className="headerText"  style={{fontSize:'1.9rem', textShadow:'2px 2px 1px black', color:'#E8C10D', userSelect:'none' }}>
-                                Welcome&nbsp;{localStorage.getItem('firstname')}!</h4>
-                            :    <h4 className="headerText"  style={{fontSize:'1.9rem', textShadow:'2px 2px 1px black', color:'#E8C10D', userSelect:'none' }}>
-                            Welcome!</h4> }
-                                <div style={{minHeight:'50px'}} onClick ={()=> {
-                                if(this.props.isAdmin){
-                                this.setState({
-                                    editText1: true,
-                                    editImg: Edit
-                                })
-                                }
-                                }}>
-                                {this.state.editText1 ?
-                            <div style={{display:'flex', flexDirection:'column', justifyContent:'center',}}
-                                onMouseLeave={()=> {
-                                    if(this.props.isAdmin){
-                                        this.setState({
-                                            editText1: false,
-                                            editImg: ''
-                                        })
-                                }}}
+               
+               {this.state.siteId ?
+                <div>
+                
+                    <div style={{ display: 'flex', flexDirection:'row', justifyContent:'center', paddingBottom:'3%',paddingTop:'5vh'}} >
+                        
+                    <Spring
+                                config={{duration: 700}} 
+                                native
+                                from={{ o: 0, marginT: '-1500px' }}
+                                to={{ o: 1, marginT: '0px'  }} 
                                 >
-                                <textarea  style={{ width:'100%', fontSize: '1.1rem',  lineHeight:'1.4rem', height:'142px', paddingLeft:'5%', paddingRight:'5%' }} defaultValue={this.state.welcomeText}
-                                onChange={(e) => {
+                                {({ o, marginT }) => (
+                                <animated.div style={{
+                                        opacity: o,
+                                        marginTop: marginT
+                                        
+                                }}>
+                        
+                        <div className="transparentContainer" style={{display:'flex',flexDirection: 'column',  marginLeft:'2%',marginRight:'2%',  background: 'rgba(26, 35, 64, 0.5)', borderRadius:'20px'}}>
+                            <div className="welcomeText" style={{padding:'2%', textAlign:'center'}} >
+
+                        
+                                <Container>
+                                    <Col className="welcomeText">
+                                <img className="logoIcon" src={LogoIcon} style={{width: '10vh', marginBottom: '1rem', marginTop: '4%',  filter:'drop-shadow(2px 2px 1px black)'}}></img>
+                                {this.props.signedIn ? 
+                                <h4  className="headerText"  style={{fontSize:'1.9rem', textShadow:'2px 2px 1px black', color:'#E8C10D', userSelect:'none' }}>
+                                    Welcome&nbsp;{localStorage.getItem('firstname')}!</h4>
+                                :    <h4 className="headerText"  style={{fontSize:'1.9rem', textShadow:'2px 2px 1px black', color:'#E8C10D', userSelect:'none' }}>
+                                Welcome!</h4> }
+                                    <div style={{minHeight:'50px'}} onClick ={()=> {
+                                    if(this.props.isAdmin){
                                     this.setState({
-                                        setWelcomeText: e.target.value
+                                        editText1: true,
+                                        editImg: Edit
                                     })
-                                }}
-                                ></textarea>
-                                    <div style={{display:'flex',flexDirection:'row', justifyContent:'center'}}>
-                                        <img style={editIcon} src= {this.state.editImg} 
-                                        onClick={() => {
-                                            this.updateSite();
-                                        }}></img>
-                                    </div>
-                            </div>
-                            :
-                            <p className='welcomeText' style={{ fontSize: '1.1rem', textShadow:'2px 2px 1px black', lineHeight:'3rem', color: 'white'}}>
-                                {this.state.welcomeText}
-                            </p>
-                            }
-                            </div>
-                            <div style={{minHeight:'25px'}}onClick ={()=> {
-                                if(this.props.isAdmin){
-                                this.setState({
-                                    editText2: true,
-                                    editImg2: Edit
-                                })
-                                }}}>
-                         {this.state.editText2 ? 
-                         <div   onMouseLeave={()=> {
-
-                            if(this.props.isAdmin){
-                                this.setState({
-                                    editText2: false,
-                                    editImg2: ''
-                            
-                                })
-                        }}}>
-                            <input defaultValue={this.state.welcomeTextOwner} className='ownerText' style={{ marginLeft:'10%', marginRight:'10%',fontSize: '1.2rem',  lineHeight:'1rem', }}
-                                  onChange={(e) => {
-                                    this.setState({
-                                        setWelcomeTextOwner: e.target.value
-                                    })
-                                }}
-                                
-                            ></input>
-                            <div style={{display:'flex',flexDirection:'row', justifyContent:'center'}}>
-                                <img style={editIcon} src= {this.state.editImg2}
-                                  onClick={() => {
-                                    this.updateSite();
-                                }}></img>
-                            </div>
-
-
-                         </div>
-                         :<p className='ownerText' style={{ marginLeft:'10%', marginRight:'10%',fontSize: '1.2rem', textShadow:'2px 2px 1px black', lineHeight:'1rem', color: '#E8C10D'}}>
-                            {this.state.welcomeTextOwner}</p>}
-                            </div>
-                            <div style={{minHeight:'50px'}}  onClick ={()=> {
-
-
-                                if(this.props.isAdmin){
-                                this.setState({
-                                    editText3: true,
-                                    editImg3: Edit
-
-                                })
-                                }}}>
-                                {this.state.editText3 ? 
-                                <div   onMouseLeave={()=> {
-
-                                if(this.props.isAdmin){
-                                this.setState({
-                                    editText3: false,
-                                    editImg3: ''
-
-                                })
-                                }}}>
-                                    <input defaultValue={this.state.welcomeTextOwnerCompany} className='ownerText' style={{ marginLeft:'10%', marginRight:'10%',fontSize: '1.2rem',  lineHeight:'1rem', }}
-                                        onChange={(e) => {
+                                    }
+                                    }}>
+                                    {this.state.editText1 ?
+                                <div style={{display:'flex', flexDirection:'column', justifyContent:'center',}}
+                                    onMouseLeave={()=> {
+                                        if(this.props.isAdmin){
                                             this.setState({
-                                                setWelcomeTextOwnerCompany: e.target.value
+                                                editText1: false,
+                                                editImg: ''
                                             })
-                                        }}
-                                    ></input>
-                                    <div style={{display:'flex',flexDirection:'row', justifyContent:'center'}}>
-                                        <img style={editIcon} src= {this.state.editImg3}
-                                        onClick={() => {
-                                            this.updateSite();
-                                        }}></img>
-                                    </div>
+                                    }}}
+                                    >
+                                    <textarea  style={{ width:'100%', fontSize: '1.1rem',  lineHeight:'1.4rem', height:'142px', paddingLeft:'5%', paddingRight:'5%' }} defaultValue={this.state.welcomeText}
+                                    onChange={(e) => {
+                                        this.setState({
+                                            setWelcomeText: e.target.value
+                                        })
+                                    }}
+                                    ></textarea>
+                                        <div style={{display:'flex',flexDirection:'row', justifyContent:'center'}}>
+                                            <img style={editIcon} src= {this.state.editImg} 
+                                            onClick={() => {
+                                                this.updateSite();
+                                            }}></img>
+                                        </div>
+                                </div>
+                                :
+                                <p className='welcomeText' style={{ fontSize: '1.1rem', textShadow:'2px 2px 1px black', lineHeight:'3rem', color: 'white'}}>
+                                    {this.state.welcomeText}
+                                </p>
+                                }
+                                </div>
+                                <div style={{minHeight:'25px'}}onClick ={()=> {
+                                    if(this.props.isAdmin){
+                                    this.setState({
+                                        editText2: true,
+                                        editImg2: Edit
+                                    })
+                                    }}}>
+                            {this.state.editText2 ? 
+                            <div   onMouseLeave={()=> {
 
+                                if(this.props.isAdmin){
+                                    this.setState({
+                                        editText2: false,
+                                        editImg2: ''
+                                
+                                    })
+                            }}}>
+                                <input defaultValue={this.state.welcomeTextOwner} className='ownerText' style={{ marginLeft:'10%', marginRight:'10%',fontSize: '1.2rem',  lineHeight:'1rem', }}
+                                    onChange={(e) => {
+                                        this.setState({
+                                            setWelcomeTextOwner: e.target.value
+                                        })
+                                    }}
+                                    
+                                ></input>
+                                <div style={{display:'flex',flexDirection:'row', justifyContent:'center'}}>
+                                    <img style={editIcon} src= {this.state.editImg2}
+                                    onClick={() => {
+                                        this.updateSite();
+                                    }}></img>
                                 </div>
-                                :<p className='ownerText' style={{ marginLeft:'10%', marginRight:'10%',fontSize: '0.9rem', textShadow:'2px 2px 1px black', lineHeight:'1rem', color: 'white'}}>
-                                {this.state.welcomeTextOwnerCompany}</p>}
+
+
+                            </div>
+                            :<p className='ownerText' style={{ marginLeft:'10%', marginRight:'10%',fontSize: '1.2rem', textShadow:'2px 2px 1px black', lineHeight:'1rem', color: '#E8C10D'}}>
+                                {this.state.welcomeTextOwner}</p>}
                                 </div>
-            
-                    </Col>
-                            </Container>
-                 
+                                <div style={{minHeight:'50px'}}  onClick ={()=> {
+
+
+                                    if(this.props.isAdmin){
+                                    this.setState({
+                                        editText3: true,
+                                        editImg3: Edit
+
+                                    })
+                                    }}}>
+                                    {this.state.editText3 ? 
+                                    <div   onMouseLeave={()=> {
+
+                                    if(this.props.isAdmin){
+                                    this.setState({
+                                        editText3: false,
+                                        editImg3: ''
+
+                                    })
+                                    }}}>
+                                        <input defaultValue={this.state.welcomeTextOwnerCompany} className='ownerText' style={{ marginLeft:'10%', marginRight:'10%',fontSize: '1.2rem',  lineHeight:'1rem', }}
+                                            onChange={(e) => {
+                                                this.setState({
+                                                    setWelcomeTextOwnerCompany: e.target.value
+                                                })
+                                            }}
+                                        ></input>
+                                        <div style={{display:'flex',flexDirection:'row', justifyContent:'center'}}>
+                                            <img style={editIcon} src= {this.state.editImg3}
+                                            onClick={() => {
+                                                this.updateSite();
+                                            }}></img>
+                                        </div>
+
+                                    </div>
+                                    :<p className='ownerText' style={{ marginLeft:'10%', marginRight:'10%',fontSize: '0.9rem', textShadow:'2px 2px 1px black', lineHeight:'1rem', color: 'white'}}>
+                                    {this.state.welcomeTextOwnerCompany}</p>}
+                                    </div>
+                
+                        </Col>
+                                </Container>
+                    
+                            </div>
                         </div>
+                        </animated.div>
+                                )}
+                            </Spring>
                     </div>
-                    </animated.div>
+            
+                    <div>
+                        
+                        <Spring
+                        config={{duration: 600, delay: 800}} 
+                                    native
+                                    from={{ o: 0 }}
+                                    to={{ o: 1}} >
+                                    {({ o }) => (
+                            
+                        <animated.div style={{
+                                            opacity: o,
+
+                                        }}>
+                            <ServicesHome/>
+                        </animated.div>
                             )}
                         </Spring>
-                </div>
-           
-                <div>
+                    </div>
                     
-                <Spring
-                config={{duration: 600, delay: 800}} 
-                            native
-                            from={{ o: 0 }}
-                            to={{ o: 1}} >
-                            {({ o }) => (
-                      
-                <animated.div style={{
-                                    opacity: o,
-
-                                }}>
-                    <ServicesHome/>
-                </animated.div>
-                    )}
-                </Spring>
-                </div>
-                <div >
-                <Spring
+                    <div >
+                    <Spring
                             config={{duration: 600, delay: 1000}} 
                             native
                             from={{ o: 0 }}
@@ -326,6 +332,10 @@ class Home extends React.Component <HomeProps, HomeTypes> {
                     )}
                 </Spring>
                 </div>
+                </div>
+               : <div>
+               <img style={{ width: '15vh',marginTop: '28vh',marginBottom: '50vh', filter: 'drop-shadow(2px 2px 2px black)'}} id = 'loadingImg' src ={LoadingGif}></img>
+              </div>  }   
             </div>
         );
     }
