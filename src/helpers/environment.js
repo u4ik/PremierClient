e9d6d11d17f544a5ac02b0d5637b5d6c5335aa43
u4ik <affirmativeamit@gmail.com>
2020-07-29 01:29:15 -0400

    background mobile

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,14 +1,32 @@
 let APIURL = '';
+let serverPort = 3000
+let frontEndUrl = 'premier-client.herokuapp.com'
+
+// alert(window.location.hostname)
+
+// switch (window.location.hostname) {
+//     case 'localhost' || '127.0.0.1':
+
+//         APIURL = 'http://localhost:' + serverPort;
+//         // alert(window.location.hostname)
+//         break;
+//     case 'premier-client.herokuapp.com':
+//         APIURL = "https://premier-server.herokuapp.com"
+//         break;
+//     case '':
+
+//         APIURL = 'http://' + window.location.hostname + ":" + serverPort
+//         break;
+
+// }
 
 
-switch (window.location.hostname) {
-    case 'localhost' || '127.0.0.1':
-        APIURL = 'http://localhost:3000';
-        // alert(window.location.hostname)
-        break;
-    case 'premier-client.herokuapp.com':
-        APIURL = "https://premier-server.herokuapp.com"
-
+if (window.location.hostname.includes('localhost') || window.location.hostname.includes('127.0.0.1')) {
+    APIURL = 'http://localhost:' + serverPort;
+} else if (window.location.hostname.includes(frontEndUrl)) {
+    APIURL = "https://premier-server.herokuapp.com"
+} else if (window.location.hostname.includes('192')) {
+    APIURL = 'http://' + window.location.hostname + ":" + serverPort
 }
 
 
