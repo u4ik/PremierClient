220c7dd1d7dcbfe063d27f4a3a4827b4ab35388b
u4ik <affirmativeamit@gmail.com>
2020-07-31 09:10:07 -0400

    admin controls deleting users

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -171,7 +171,7 @@ class Home extends React.Component<HomeProps, HomeTypes> {
 
                                     }}>
 
-                                        <div className="transparentContainer" style={{ display: 'flex', flexDirection: 'column', marginLeft: '-5%', marginRight: '-5%', background: 'rgba(26, 35, 64, 0.5)', borderRadius: '20px' }}>
+                                        <div className="transparentContainer" style={{ display: 'flex', flexDirection: 'column', marginLeft: '5%', marginRight: '5%', background: 'rgba(26, 35, 64, 0.5)', borderRadius: '20px' }}>
                                             <div className="welcomeText" style={{ padding: '2%', textAlign: 'center' }} >
 
 
